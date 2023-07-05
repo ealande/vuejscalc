76c789f31daf0d917c54c3131100f8a5bf6fcdf2
DevErickLima <116379126+ealande@users.noreply.github.com>
2023-07-05 10:43:31 -0300

    Update App.vue
    Implementing the v-model bind. And fixing the two-way data binding. Adding the function result and fixing the problems with the display of the result. The variable operator was inserted inside the reactive const. The select tag was fixed.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
 <script setup>
 import { reactive } from 'vue';
-
 const title = "First VueJS Calculator";
-
 const state = reactive(
   {
-    number1: '',
-    number2: '',
+    number1: "",
+    number2: "",
+    operator:""
   }
 );
 
-function result(){
-  return parseInt(state.number2) + parseInt(state.number1);
+function result() {
+if (state.operator === "sum") {
+return sum()
+} else if (state.operator === "minus") {
+return minus()
+}else if (state.operator === "multi") {
+return multi()
+}else if (state.operator === "divide") {
+return divide()
+} else (state.operator === '');
+return ""
 }
+
 
 function insertNumber1(event){
   state.number1 = event.target.value
 }
-
 function insertNumber2(event){
   state.number2 = event.target.value
 }
 
-function sum(event) {
-
+function sum (){
+   return parseInt(state.number1) + parseInt(state.number2)
 }
+function minus (){
+   return state.number1 - state.number2
+}
+function multi (){
+   return state.number1 * state.number2
+}
+function divide (){
+   return state.number1 / state.number2
+}
+
 </script>
 
 <template>
@@ -33,19 +51,18 @@ function sum(event) {
   </header>
   <main>
     <p>{{ state.number1 }}</p> <br/>
-    <input type="number" @keyup="insertNumber1">
-    <select>
-      <option value="+"> + </option>
-      <option value="-"> - </option>
-      <option value="*"> * </option>
-      <option value="/"> / </option>
+    <input type="number" @keyup="insertNumber1"> <br/><br/>
+    <select v-model="state.operator">
+      <option value="sum"> + </option>
+      <option value="minus"> - </option>
+      <option value="multi"> * </option>
+      <option value="divide"> / </option>
     </select>
     <p>{{ state.number2 }}</p> <br/>
     <input type="number" @keyup="insertNumber2">
-    <p v-if="state.number1 && state.number2">The result is {{ result() }}</p>
+    <p v-if="state.number1 !== '' && state.number2 !== ''">The result is {{ result() }}</p>
   </main>
 </template>
 
 <style scoped>
-
 </style>
